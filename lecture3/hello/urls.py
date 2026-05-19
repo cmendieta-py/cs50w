471ca11a165ee55765b586b2196d28222a367408
claudioProgramer <claualfa666@gmail.com>
2026-05-19 08:03:50 -0300

    correpcion carpetas

--- lecture3/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("greet/<str:name>", views.greet, name="greet"),
-    path("lepe", views.lepe, name="lepe"),
-    path("david", views.david, name="david")
-]
--- lecture4/airline/flights/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Airport, Flight
-
-admin.site.register(Airport)
-admin.site.register(Flight)
