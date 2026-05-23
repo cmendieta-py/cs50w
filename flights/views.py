ddc4e99c7eb086f2af8ec3969cfc467d46573465
claudioProgramer <claualfa666@gmail.com>
2026-05-23 19:15:54 -0300

    sistema login de ejemplo terminado

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Flight
+from .models import Passenger
 
 # Create your views here.
 def index(request):
@@ -10,9 +13,22 @@ def index(request):
     })
 
 def flight(request, flight_id):
+    
     flight = Flight.objects.get(pk=flight_id)
     passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
+
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": passengers
+        "passengers": passengers,
+        "non_passengers": non_passengers
     })
+
+def book(request, flight_id):
+    
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+
+        return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
